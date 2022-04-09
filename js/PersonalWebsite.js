ff1430012226b51192916426a63d8aa16ea7f830
nivethanin <nivethanin@gmail.com>
2022-04-09 18:22:49 -0400

    update

--- js/PersonalWebsite.js
+++ js/PersonalWebsite.js
@@ -1,14 +1,35 @@
 
-window.onscroll = function() {myFunction()};
+//Collabsible buttons on the projects page
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
+
+window.onscroll = function () { sticker() };
 
 
 var topnav = document.getElementById("topnav");
 var sticky = topnav.offsetTop;
 
-function myFunction() {
+
+function sicker() {
   if (window.pageYOffset >= sticky) {
     topnav.classList.add("sticky")
   } else {
     topnav.classList.remove("sticky");
   }
 }
+
